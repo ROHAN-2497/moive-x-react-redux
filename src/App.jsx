061b1eb619f3ad733c0d025a27fa850df065336a
Rohan_Islam <rohanislam3669@gmail.com>
2024-04-21 06:12:30 +0600

    api setup and  api testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import "./App.css";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/person/popular").then((res) => {
+      console.log(res);
+    });
+  };
+
   return <>app</>;
 }
 
