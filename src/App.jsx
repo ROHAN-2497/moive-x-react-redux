d80003004b0806446d91a608b6393cc22c39d3f6
Rohan_Islam <rohanislam3669@gmail.com>
2024-04-25 14:33:44 +0600

    op

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import "./App.css";
 import { fetchDataFromApi } from "./utils/api";
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfigaration } from "./store/homeSlice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import pageNotFound from "./pages/404/pageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,10 +28,17 @@ function App() {
   };
 
   return (
-    <>
-      app <br />
-      pages: {url?.total_pages}
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<pageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
@@ -7,8 +6,6 @@ import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Provider>
 );
