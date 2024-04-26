e702977e31fc45cbbd817d8566ed58af5746d884
Rohan_Islam <rohanislam3669@gmail.com>
2024-04-27 03:51:37 +0600

    kk

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
 const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
   return (
     <div className="heroBanner">
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <span className="title">Welcome.</span>
+        <span className="subTitle">
+          Millions of movies, TV Shows and people to discover. Explore now.
+        </span>{" "}
+        <div className="searchInput">
+          <input
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyUp={searchQueryHandler}
+            type="text"
+            placeholder="Search for a movie or tv show...."
+          />
+          <button>Search</button>
+        </div>
+      </div>
     </div>
   );
 };
